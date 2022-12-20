a719dc33d9b60ba6ad299bf37372e677cec66953
Jenya Hutsul <jenyahutsul@gmail.com>
2022-12-20 14:29:49 +0200

    added ease transition and other

--- Gallery/Gallery.js
+++ Gallery/Gallery.js
@@ -1,6 +1,11 @@
 const GalleryClassName = 'gallery';
 const GalleryLineClassName = 'gallery-line';
-const GallerySlideClassName = 'gallery-slide'
+const GallerySlideClassName = 'gallery-slide';
+const GalleryDotsClassName = 'galerry-dots';
+const GalleryDotActiveClassName = 'galerry-dot-active';
+const GalleryNavClassName = 'gallery-nav';
+const GalleryNavLeftClassName = 'gallery-nav-left';
+const GalleryNavRightClassName = 'gallery-nav-right';
 
 class Gallery {
     constructor(element, options = {}) {
@@ -8,6 +13,10 @@ class Gallery {
         this.size = element.childElementCount;
         this.currentSlide = 0;
         this.lineNode = '';
+        this.currentSlideWasChanged = false;
+        this.settings = {
+            margin : options.margin || 0,
+        }
 
         this.manageHTML = this.manageHTML.bind(this);
         this.setParams = this.setParams.bind(this);
@@ -46,14 +55,16 @@ class Gallery {
     }
 
     setParams(){
+        this.maximumX = -(this.size - 1) * (this.width + this.settings.margin);
         const coordsContainer = this.containerNode.getBoundingClientRect();
         this.width = coordsContainer.width;
-        this.x = -this.currentSlide * this.width;
-
-        this.lineNode.style.width = `${this.size * this.width}px`
+        this.x = -this.currentSlide * (this.width + this.settings.margin);
+        
+        this.lineNode.style.width = `${this.size * (this.width + this.settings.margin)}px`
         
         Array.from(this.slideNodes).forEach(slideNode => {
             slideNode.style.width = `${this.width}px`;
+            slideNode.style.marginRight = `${this.settings.margin}px`;
         })
     }
 
@@ -65,34 +76,67 @@ class Gallery {
         window.addEventListener('pointerup', this.stopDrag);
     }
 
-    startDrag(){
+    startDrag(evt){
+        this.currentSlideWasChanged = false;
+        this.clickX = evt.pageX
+        this.startX = this.x;
         window.addEventListener('pointermove', this.dragging);
     }
 
-    stopDrag(evt){
-        this.clickX = evt.pageX; 
-        
+    stopDrag(){ 
         window.removeEventListener('pointermove', this.dragging);
+        this.x = -this.currentSlide * (this.width + this.settings.margin);
+        this.setStylePosition();
+        this.setStyleTransition();
     }
 
     dragging(evt){
+        this.resetStyleTransition();
         this.dragX = evt.pageX;
         const dragShift = this.dragX - this.clickX;
+        const easing = dragShift / 5;
+        this.x = Math.max(Math.min(this.startX + dragShift, easing), this.maximumX + easing);
+        this.setStylePosition();
 
-        this.x = dragShift;
-        this.setStylePosition()
+        if(
+            dragShift > 20 && dragShift > 0 && 
+            !this.currentSlideWasChanged && 
+            this.currentSlide > 0
+        ){
+            this.currentSlideWasChanged = true;
+            this.currentSlide = this.currentSlide - 1;
+        }
+
+        if(
+            dragShift < -20 && dragShift < 0 && 
+            !this.currentSlideWasChanged &&
+            this.currentSlide < this.size - 1
+        ){
+            this.currentSlideWasChanged = true;
+            this.currentSlide = this.currentSlide + 1;
+        }
     }
 
-    setStylePosition(shift){
+    setStylePosition(){
         this.lineNode.style.transform = `translate3d(${this.x}px, 0, 0)` 
     }
 
     destroyEvents(){
         window.removeEventListener('resize', this.debounceRisezeGallery);
+        this.lineNode.removeEventListener('pointerdown', this.startDrag);
+        window.removeEventListener('pointerup', this.stopDrag)
     }
 
     resizeGallery(){
         this.setParams();
+    }
+
+    setStyleTransition(){
+        this.lineNode.style.transition = `all 0.25s ease 0s`;
+    }
+
+    resetStyleTransition(){
+        this.lineNode.style.transition = `all 0s ease 0s`;
     }
 }
 //Helpers
